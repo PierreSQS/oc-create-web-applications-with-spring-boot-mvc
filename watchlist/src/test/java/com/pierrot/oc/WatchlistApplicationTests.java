ece29317b5d6734a81c2a88a06866ec6cfcbc669
PierreSQS <nomo_messina@web.de>
2020-09-07 17:49:15 +0200

    Commit 01 OK: Initial Commit

---
+++ watchlist/src/test/java/com/pierrot/oc/WatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pierrot.oc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
