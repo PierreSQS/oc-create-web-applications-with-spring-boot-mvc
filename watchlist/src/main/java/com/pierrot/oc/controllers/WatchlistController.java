e7213238a91a4134eda68035815268c5b0e5511e
PierreSQS <nomo_messina@web.de>
2020-09-08 20:04:01 +0200

    Commit 06-NOK: Extended Controller and Tests for Watchlist item form
    processing and submission. Also adapted the HTML-Template

--- watchlist/src/main/java/com/pierrot/oc/controllers/WatchlistController.java
+++ watchlist/src/main/java/com/pierrot/oc/controllers/WatchlistController.java
@@ -7,7 +7,9 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.pierrot.oc.entities.WatchlistItem;
 
@@ -24,11 +26,15 @@ public class WatchlistController {
 		watchItemsList = new ArrayList<>();
 
 		// initializing the data of the model
-		watchItemsList.clear();
-		watchItemsList.add(new WatchlistItem("The Godfather", "8.5", "high", "this is a must!"));
-		watchItemsList.add(new WatchlistItem("Le clan des siciliens", "8.0", "high", "a french mus"));
-		watchItemsList.add(new WatchlistItem("Live and let die", "8.5", "high", "this is a must!"));
-		watchItemsList.add(new WatchlistItem("Tatort", "2.5", "high", "booh to the germans!"));
+		// watchItemsList.clear();
+		// watchItemsList.add(new WatchlistItem("The Godfather", "8.5", "high", "this is
+		// a must!"));
+		// watchItemsList.add(new WatchlistItem("Le clan des siciliens", "8.0", "high",
+		// "a french mus"));
+		// watchItemsList.add(new WatchlistItem("Live and let die", "8.5", "high", "this
+		// is a must!"));
+		// watchItemsList.add(new WatchlistItem("Tatort", "2.5", "high", "booh to the
+		// germans!"));
 
 		Map<String, Object> model = new HashMap<>();
 
@@ -41,4 +47,35 @@ public class WatchlistController {
 
 	}
 
+	@GetMapping("/watchlistItemForm")
+	public ModelAndView showWatchListForm() {
+		// the view Name
+		String viewName = "watchlistItemForm";
+
+		// initializing the model and the data
+		Map<String, Object> model = new HashMap<>();
+
+		// initializing data with empty object -> empty form
+		model.put("watchlistItem", new WatchlistItem());
+
+		return new ModelAndView(viewName, model);
+
+	}
+
+	@PostMapping("/watchlistItemForm")
+	public ModelAndView submitWatchlistItemForm(WatchlistItem watchlistItem) {
+
+		// initializing the data
+		watchItemsList.add(watchlistItem);
+
+		// initializing the model
+		Map<String, Object> model = new HashMap<>();
+		model.put("watchItemsList", watchItemsList);
+
+		// Redirect to /watchlist
+		RedirectView redirectView = new RedirectView("/watchlist");
+
+		return new ModelAndView(redirectView, model);
+
+	}
 }
--- watchlist/src/test/java/com/pierrot/oc/controllers/WatchlistControllerTest.java
+++ watchlist/src/test/java/com/pierrot/oc/controllers/WatchlistControllerTest.java
@@ -2,8 +2,9 @@ package com.pierrot.oc.controllers;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -20,18 +21,36 @@ class WatchlistControllerTest {
 	private MockMvc mockMvc;
 	
 	@Test
-	void contextLoad() {
-		
-	}
-
-	@Test
 	void testGetWatchList() throws Exception {
+		mockMvc.perform(get("/watchlistItemForm"))
+			.andExpect(view().name("watchlistItemForm"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("watchlistItem"))
+			.andExpect(content().string(containsString("Submit an item")));
+//			.andDo(print());
+	}
+	
+	@Test
+	void testShowWatchListItemForm() throws Exception {
 		mockMvc.perform(get("/watchlist"))
 			.andExpect(view().name("watchlist"))
 			.andExpect(status().isOk())
 			.andExpect(model().attributeExists("numberOfMovies","watchlistItems"))
-			.andExpect(content().string(containsString("The Godfather")))
-			.andDo(print());
+			.andExpect(content().string(containsString("The Godfather")));
+//			.andDo(print());
+	}
+
+	@Test
+	void testSubmitWatchListItemForm() throws Exception {
+		mockMvc.perform(post("/watchlistItemForm")
+				.param("title", "the Prinz of Zamunda")
+				.param("rating", "8.0")
+				.param("priority", "High")
+				.param("comment", "the coolest Eddie Murphy"))		
+			.andExpect(status().is3xxRedirection())
+			.andExpect(model().size(1))
+			.andExpect(content().string(containsString("")));
+//			.andDo(print());
 	}
 
 }
