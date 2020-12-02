b017043679a1031be23fa9049e70d41f3d30dc7f
PierreSQS <nomo_messina@web.de>
2020-12-02 15:35:13 +0100

    Commit 04-jpa-OK: Refactoring to only Java Bean Configuration. Tests OK

--- watchlist/src/main/java/com/pierrot/oc/configuration/WatchlistAppConfiguration.java
+++ watchlist/src/main/java/com/pierrot/oc/configuration/WatchlistAppConfiguration.java
@@ -1,10 +1,43 @@
 package com.pierrot.oc.configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+
+import com.pierrot.oc.entities.WatchlistItem;
+import com.pierrot.oc.repositories.impl.WatchlistRepositoryImpl;
+import com.pierrot.oc.repositories.interfaces.WatchlistRepository;
+import com.pierrot.oc.services.MovieRatingService;
+import com.pierrot.oc.services.WatchlistService;
 
 @Configuration
-@ImportResource(locations = {"classpath:com/pierrot/oc/**/*-config.xml"})
 public class WatchlistAppConfiguration {
+	
+	private List<WatchlistItem> itemList;
+	
+	@Autowired
+	private WatchlistRepository watchlistRepo;
+
+	@Autowired
+	private MovieRatingService movieRatingServ;
+	
+	@Bean
+	public WatchlistRepository createWatchlistRepo() {
+		itemList = new ArrayList<>();
+		return new WatchlistRepositoryImpl(itemList);
+	}
+	
+	@Bean
+	public MovieRatingService createMovieRatingService() {
+		return new MovieRatingService();
+	}
+	
+	@Bean
+	public WatchlistService createWatchlistService() {
+		return new WatchlistService(watchlistRepo, movieRatingServ);
+	}	
 
 }
