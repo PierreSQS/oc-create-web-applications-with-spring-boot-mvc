7216fb9abc04d2c40f637c69bf2666ddb762268b
PierreSQS <nomo_messina@web.de>
2020-12-02 02:32:54 +0100

    Commit 03-jpa-OK: Now all the Tests are passing!!!!!

--- watchlist/src/main/java/com/pierrot/oc/configuration/WatchlistAppConfiguration.java
+++ watchlist/src/main/java/com/pierrot/oc/configuration/WatchlistAppConfiguration.java
@@ -4,11 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-// DOESN'T WORK!!!
-//@ImportResource(locations = {"classpath*:/*-config.xml"})
-//DOESN'T WORK TOO!!!
-//@ImportResource(locations = {"classpath:com/pierrot/oc/*-config.xml"})
-
 @ImportResource(locations = {"classpath:com/pierrot/oc/**/*-config.xml"})
 public class WatchlistAppConfiguration {
 
--- watchlist/src/test/java/com/pierrot/oc/WatchlistApplicationTests.java
+++ watchlist/src/test/java/com/pierrot/oc/WatchlistApplicationTests.java
@@ -2,8 +2,12 @@ package com.pierrot.oc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.pierrot.oc.configuration.WatchlistAppConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = {WatchlistAppConfiguration.class}) // Notice the Annotation!!!
 class WatchlistApplicationTests {
 
 	@Test
